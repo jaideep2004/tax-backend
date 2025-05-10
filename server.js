f817fac7fcdc416de40c90495fb27c25847a221b
unknown <jaisidhu2004@gmail.com>
2025-05-10 22:02:35 +0530

    10 may

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 // app.listen(PORT, () => {
 // 	console.log(`Server running on port ${PORT}`);
 // });
- 
+
 const options = {
 	key: fs.readFileSync(path.join(__dirname, "certs/privkey.pem")),
 	cert: fs.readFileSync(path.join(__dirname, "certs/fullchain.pem")),
